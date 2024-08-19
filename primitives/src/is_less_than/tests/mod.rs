db80b976e5f4b0d73616767b258218bc8575777a
Alexander Golovanov <Golovanov12345@gmail.com>
2024-08-19 23:10:30 +0200

    fix: Add a test that breaks IsLessThan

--- primitives/src/is_less_than/tests/mod.rs
+++ primitives/src/is_less_than/tests/mod.rs
@@ -4,7 +4,7 @@ use afs_stark_backend::{prover::USE_DEBUG_BUILDER, verifier::VerificationError};
 use afs_test_utils::config::baby_bear_poseidon2::run_simple_test_no_pis;
 use p3_baby_bear::BabyBear;
 use p3_field::AbstractField;
-use p3_matrix::dense::DenseMatrix;
+use p3_matrix::dense::{DenseMatrix, RowMajorMatrix};
 
 use super::{super::is_less_than::IsLessThanChip, columns::IsLessThanCols};
 use crate::{is_less_than::IsLessThanAir, range_gate::RangeCheckerGateChip};
@@ -80,6 +80,43 @@ fn test_is_less_than_negative() {
     let range_trace = chip.range_checker.generate_trace();
 
     trace.values[2] = AbstractField::from_canonical_u64(0);
+
+    USE_DEBUG_BUILDER.with(|debug| {
+        *debug.lock().unwrap() = false;
+    });
+    assert_eq!(
+        run_simple_test_no_pis(
+            vec![&chip.air, &chip.range_checker.air],
+            vec![trace, range_trace],
+        ),
+        Err(VerificationError::OodEvaluationMismatch),
+        "Expected verification to fail, but it passed"
+    );
+}
+
+#[test]
+fn test_lt_chip_lower_overflows_babybear() {
+    let bus_index: usize = 0;
+    let max_bits: usize = 30;
+    let decomp: usize = 8;
+    let range_max: u32 = 1 << decomp;
+
+    let range_checker = Arc::new(RangeCheckerGateChip::new(bus_index, range_max));
+
+    let chip = IsLessThanChip::new(bus_index, max_bits, decomp, range_checker);
+    let correct_trace_row = chip
+        .generate_trace(vec![((1 << 27) - 2, 1 << 30)])
+        .values
+        .to_vec();
+    let fake_trace_row = [(1 << 27) - 2, 1 << 30, 0, 0, 0, 0, 0, 0, 0]
+        .map(BabyBear::from_canonical_u32)
+        .to_vec();
+    let trace = [correct_trace_row, fake_trace_row].concat();
+    let trace = RowMajorMatrix::new(trace.to_vec(), 9);
+    for _ in 0..5 {
+        chip.range_checker.add_count(0);
+    }
+    let range_trace = chip.range_checker.generate_trace();
 
     USE_DEBUG_BUILDER.with(|debug| {
         *debug.lock().unwrap() = false;
